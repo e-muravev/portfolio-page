906f2708d8bd8f146647de5fdda669394661de96
e-muravev <e.muravev.js@gmail.com>
2020-05-24 19:50:00 +0700

    changed myproject page

--- src/MyProjects/MyProjects.js
+++ src/MyProjects/MyProjects.js
@@ -3,6 +3,9 @@ import { useHistory } from "react-router-dom"
 import NextButton from '../NextButton/NextButton'
 import PrevButton from '../PrevButton/PrevButton'
 import faceDetect from './face detect.jpg'
+import filmSearch from './film search.jpg'
+import weatherApp from './weather app.jpg'
+import portfolio from './portfolio.jpg'
 
 
 function MyProjects() {
@@ -40,26 +43,37 @@ function MyProjects() {
                 <div style={{width: '70%'}}>
                     <h2>Weather App (Noncommercial)</h2>
                     <div style={{position: 'relative'}}>
-                        <img style={{width: '100%'}} src={faceDetect} alt="facedetect"/>
+                        <img style={{width: '100%'}} src={weatherApp} alt="weather app"/>
                         <button style={{position: 'absolute', bottom: 4, right: 0}}
-                                onClick={()=>goExternURL('https://e-muravev.github.io/face-recognition-app/')}>
+                                onClick={()=>goExternURL('https://e-muravev.github.io/weather-app/')}>
                         See Live
                         </button>
                     </div> 
                     <p>Funny application which can be used for recognizing weather in any location in the world.</p>
                     <p>I used React,  React.Hooks and Weather API(OpenWeatherMap) in order to create this website.</p>
+                    <p>And yes of course it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
                 </div>
                 <div style={{width: '70%'}}>
                     <h2>Film or Game Search App (Noncommercial)</h2>
                     <div style={{position: 'relative'}}>
-                        <img style={{width: '100%'}} src={faceDetect} alt="facedetect"/>
+                        <img style={{width: '100%'}} src={filmSearch} alt="film search"/>
                         <button style={{position: 'absolute', bottom: 4, right: 0}}
-                                onClick={()=>goExternURL('https://e-muravev.github.io/face-recognition-app/')}>
+                                onClick={()=>goExternURL('https://e-muravev.github.io/films-games-search-app/')}>
                         See Live
                         </button>
                     </div>
                     <p>Awesome application which allows you to find out information about any film or game.</p>
                     <p>I used React,  React.Hooks and OMDB API(Open Movies Data Base) in order to create this website.</p>
+                    <p>And again it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
+                </div>
+                <div style={{width: '70%'}}>
+                    <h2>My Portfolio Site (Read about stack I used to create this)</h2>
+                    <div style={{position: 'relative'}}>
+                        <img style={{width: '100%'}} src={portfolio} alt="portfolio"/>
+                    </div>
+                    <p>Awesome page which gives you information about me.</p>
+                    <p>I used React, React Router and React.Hooks in order to create this website.</p>
+                    <p>What about adaptive? Sure, it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
                 </div>
                 <PrevButton goPrevious={goPrevious} />
                 <NextButton goNext={goNext} opacity={100} cursor={'pointer'}/>
@@ -86,7 +100,7 @@ function MyProjects() {
                 <div style={{width: '90%'}}>
                     <h4>Weather App (Noncommercial)</h4>
                     <div style={{position: 'relative'}}>
-                        <img style={{width: '100%'}} src={faceDetect} alt="facedetect"/>
+                        <img style={{width: '100%'}} src={weatherApp} alt="weather app"/>
                         <button style={{position: 'absolute', bottom: 4, right: 0, fontSize: '0.75rem'}}
                                 onClick={()=>goExternURL('https://e-muravev.github.io/face-recognition-app/')}>
                         See Live
@@ -94,18 +108,25 @@ function MyProjects() {
                     </div>  
                     <p>Funny application which can be used for recognizing weather in any location in the world.</p>
                     <p>I used React,  React.Hooks and Weather API(OpenWeatherMap) in order to create this website.</p>
+                    <p>And yes of course it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
                 </div>
                 <div style={{width: '90%'}}>
                     <h4>Film or Game Search App (Noncommercial)</h4>
                     <div style={{position: 'relative'}}>
-                        <img style={{width: '100%'}} src={faceDetect} alt="facedetect"/>
-                        <button style={{position: 'absolute', bottom: 4, right: 0, fontSize: '0.75rem'}}
-                                onClick={()=>goExternURL('https://e-muravev.github.io/face-recognition-app/')}>
-                        See Live
-                        </button>
+                        <img style={{width: '100%'}} src={filmSearch} alt="film search"/>
                     </div>  
                     <p>Awesome application which allows you to find out information about any film or game.</p>
                     <p>I used React,  React.Hooks and OMDB API(Open Movies Data Base) in order to create this website.</p>
+                    <p>And again it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
+                </div>
+                <div style={{width: '90%'}}>
+                    <h2>My Portfolio Site (Read about stack I used to create this)</h2>
+                    <div style={{position: 'relative'}}>
+                        <img style={{width: '100%'}} src={portfolio} alt="portfolio"/>
+                    </div>
+                    <p>Awesome page which gives you information about me.</p>
+                    <p>I used React, React Router and React.Hooks in order to create this website.</p>
+                    <p>What about adaptive? Sure, it has adaptive and crossbrowser behavior from mobile to laptop with HiDPI screen.</p>
                 </div>
             </div>
             );
